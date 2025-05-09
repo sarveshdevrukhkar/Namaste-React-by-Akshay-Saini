9ec6fab6ef9c7ec44628c7432285ed9c6f9dd991
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2025-05-09 15:52:44 +0530

    Modified folder structure and separate code.

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", { title: "this is React you know?", id: "heading" }, "Namaste Akshay from React.");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
