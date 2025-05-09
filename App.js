bbe85cb7700900de083f15da47eb1fa2addc7c89
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2025-05-09 20:42:48 +0530

    Creating Sibiling Elements using React.

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 /**
- * // Create Nested Elements using React.
+ * // Create Sibiling Elements using React.
  *
  * <div id="parent">
- *      <div id="child">
+ *      <div id="child1">
  *          <h1>I am H1.</h1>
+ *      </div>
+ *      <div id="child2">
+ *          <h2>I am H2.</h2>
  *      </div>
  * </div>
  */
 
-const heading = React.createElement(
-    "div",
-    { id: "parent" },
-    React.createElement("div", { id: "child" }, React.createElement("h1", {}, "I am H1")),
-);
+const heading = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" }, React.createElement("h1", {}, "I am H1")),
+    React.createElement("div", { id: "child2" }, React.createElement("h2", {}, "I am H2")),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
