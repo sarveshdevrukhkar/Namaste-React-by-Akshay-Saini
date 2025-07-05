3322384eb580ae613c1e3624b50351e0df01a9f4
sarveshdevrukhkar <79737447+SAD0XER@users.noreply.github.com>
2025-07-05 14:50:43 +0530

    React Component example.

--- App.js
+++ App.js
@@ -3,4 +3,25 @@ import ReactDOM from "react-dom/client";
 // React Element Example.
 const button = <button>This is React Element</button>;
 
-ReactDOM.createRoot(document.getElementById("root")).render(button);
+// * React Component Example.
+function ReactComponent1() {
+	return <kbd>This is React Component example.</kbd>;
+}
+
+function ReactComponent2() {
+	return (
+		<div>
+			<p>This is React Component.</p>
+			<small>Writing multiple React Elements.</small>
+		</div>
+	);
+}
+
+// Components with Arrow Function.
+const ReactComponentArrow1 = () => <p>This is React Component.</p>;
+
+const ReactComponentArrow2 = () => {
+	return <p>This is React Component.</p>;
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<ReactComponent1 />);
