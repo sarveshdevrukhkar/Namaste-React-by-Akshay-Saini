a62623da0ce011c7b2ddbf5fa01e1e5b5cba45c9
SARVPRIYAPALLI <sarveshdevrukhkar@outlook.com>
2025-08-07 17:01:40 +0530

    Learned 1st React Hook named `useState()`.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
 import { resDataList } from "../utils/mockData";
-const a = 213;
+import { useState } from "react";
+
 export default function Body() {
-    console.log(a);
+	// const [restaurantOfList, setRestaurantOfList] = useState(resDataList); // 1. Method One
+
+	// 2. Method Two
+	const useStateArray = useState(resDataList);
+	// const [restaurantOfList, setRestaurantOfList] = useStateArray; // Another way to destructure on fly.
+
+	const restaurantOfList = useStateArray[0];
+	const setRestaurantOfList = useStateArray[1];
+
+	/* These 2 methods are same. */
+
 	return (
 		<main className="body">
-			<div className="search-container">
-				<input placeholder="Search" />
-				<button className="search-button">Search</button>
+			<div className="filter-container">
+				<button
+					className="filter-btn"
+					onClick={() => {
+						const filteredData = restaurantOfList.filter((restaurant) => restaurant.info.avgRating > 4.5);
+						setRestaurantOfList(filteredData);
+					}}>
+					Top Rated Restaurant
+				</button>
 			</div>
 			<div className="restaurant-container">
-				{resDataList.map((restaurant) => (
+				{restaurantOfList.map((restaurant) => (
 					<RestaurantCard key={restaurant.info.id} resData={restaurant} />
 				))}
 			</div>
